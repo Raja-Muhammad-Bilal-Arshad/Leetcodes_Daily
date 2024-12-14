70ed4617e1af846e7edb8858af6542183bd20311
Raja Muhammad Bilal Arshad <bilalarshad.code@gmail.com>
2024-12-14 17:46:06 +0500

    Time: 0 ms (100%), Space: 10.3 MB (8.08%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        for(int r=0;r<n;r++)
+    void transpose(vector<vector<int>>& m)
+    {
+        int n=m.size();
+        for(int i=0;i<n;i++)
         {
-            for(int c=r;c<n;c++)
+            for(int j=0;j<n;j++)
             {
-                swap(matrix[r][c],matrix[c][r]);
+                if(j>i)
+                {
+                    swap(m[i][j],m[j][i]);
+                }
             }
         }
-        for(int r=0;r<n;r++)
-        { int left=0, right=n-1;
-            while(left<right)
+    }
+    void rotate(vector<vector<int>>& matrix) {
+        transpose(matrix);
+        int n=matrix.size();
+        int i=0,j=n-1;
+        while(i<j)
+        {
+            for(int k=0;k<n;k++)
             {
-                swap(matrix[r][left],matrix[r][right]);
-                left++;right--;
+                swap(matrix[k][i],matrix[k][j]);
             }
-        }
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                cout << matrix[r][c] << " ";
-            }
-            cout << endl;
+            i++;
+            j--;
         }
     }
 };
